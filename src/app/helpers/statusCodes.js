529f80a97a91ad69b1019c0d0df88ec1e8f413b7
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2021-09-12 13:56:28 +0200

    status code

---
+++ src/app/helpers/statusCodes.js
@@ -0,0 +1,14 @@
+
+export const successCodes = {
+    ok: 200,
+    created: 201
+}
+
+export const failureCodes = {
+    badRequest : 400,
+    unAuthorized : 401,
+    forbidden : 403,
+    notFound : 404,
+    conflict : 409,
+    internalServerError : 500
+}
