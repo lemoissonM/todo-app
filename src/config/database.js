9372e4ed89f6aa445b297c0e937c8cc680d559a9
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2021-09-12 13:40:09 +0200

    config db

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,33 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+require('dotenv').config();
+
+const {
+  DB_NAME,
+  DB_HOST,
+  DB_USERNAME,
+  DB_PORT,
+  DB_PASSWORD,
+  DB_UL
+} = process.env
+
+module.exports = {
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'mysql'
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    url: DB_UL,
+    dialect: 'mysql'
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: 'mysql'
   }
 }
+
