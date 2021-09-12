9ec336266155650ae72719c312e37941564c53d6
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2021-09-12 14:12:41 +0200

    messages helper

---
+++ src/app/helpers/message.helpers.js
@@ -0,0 +1,28 @@
+export const successMessages = {
+    accountCreate : "Votre compte a été créé avec success !",
+    todoCreate : "La tâche a été créée avec success !",
+    loginSuccess : "Connecté avec succès!",
+    loggedOut : "Déconnecté avec succès!",
+    recordFound : "Enregistrement récupéré avec succès !",
+    updateSuccess : "Enregistrements mis à jour avec succès!",
+    deleteRecordSuccess: "Enregistrement supprimé avec succès!",
+    completeTodo : "la tâche a été completée avec success ! ",
+    welcome : "Welcome to Immob Company API",
+}
+
+export const errorMessages ={
+    accountFailedToCreate: 'Désolé !\nLe compte n\'a pas été créé, une erreur s\'est produite. Veuillez réessayer !',
+    loginFail: 'Désolé !\nVous avez fourni un mauvais numero ou un mot de passe erroné, veuillez réessayer !',
+    noRecordFound: 'Aucune données enregistrement n\'a été trouvé !',
+    deleteRecordFail: 'Échec de la suppression de l\'enregistrement, réessayez !',
+    todoCreateFail: 'Échec de la création de la tâche, réessayez !',
+    updateFail: 'Échec de la mise à jour de l\'enregistrement, veuillez réessayer !',
+    recordCreateFail: 'Échec de la création de l\'enregistrement, veuillez réessayer !',
+    passwordFail:'Mot de passe incorrect, veuillez réessayer !!',
+    phoneFail: 'Désolé !\nVous avez fourni un mauvais numero, veuillez réessayer !',
+    fieldValidation : 'Veuillez remplir tous les champs obligatoires',
+    duplicatedPhone: 'le numero entrer a déjà été pris !',
+    duplicatedTodo: 'la tâche entrer existe déjà est n\'est pas encore finie!',
+    duplicatedEmail: 'l\'email entrer a déjà été pris !',
+    interError: 'Une Erreur interne est survenue',
+}
