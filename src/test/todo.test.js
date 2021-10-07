866a8acb0cb1ce36a82b2575c8f1617dd6fc14df
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-10-07 14:29:05 +0200

    testing methode get

---
+++ src/test/todo.test.js
@@ -0,0 +1,29 @@
+
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../bin/index';
+
+// style assertion
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Todo Api', ()=>{
+    
+    // Test route GET
+    describe('GET-ROUTE api/todo/view-all', ()=>{
+        it(' Récupération de l\'enregistrement avec succès !', (done)=>{
+            chai.request(server)
+                .get('/api/todo/view-all')
+                .end((err,res)=>{
+                    res.should.have.status(200);
+                    res.body.should.have.a('array');
+                    done();
+                })
+        })
+    })
+    // Test route GET by ID
+    // Test route POST
+    // Test route PUT
+    // Test route DELETE 
+});
